5b17676a3a7d3f7766b8f1a56353c196e86cd985
lidwan <contact@loayidwan.com>
2024-09-22 13:46:19 +0300

    Added dark/light themes

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function changeTheme() {
+    const root = document.documentElement;
+    const newTheme = root.className === 'dark' ? 'light' : 'dark';
+    root.className = newTheme;
+}
+
+document.querySelector(".themechangebtn").addEventListener("click", changeTheme);
