50644c355166bc187026ebb932364d7cf3eaa44b
lidwan <contact@loayidwan.com>
2024-09-22 13:52:33 +0300

    nvm, ppl who want light mode.. just don't

--- js/main.js
+++
@@ -1,7 +0,0 @@
-function changeTheme() {
-    const root = document.documentElement;
-    const newTheme = root.className === 'dark' ? 'light' : 'dark';
-    root.className = newTheme;
-}
-
-document.querySelector(".themechangebtn").addEventListener("click", changeTheme);
