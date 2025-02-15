51fc941f3559410e2de0c9aac5237be26dcef7da
lidwan <contact@loayidwan.com>
2025-02-15 18:20:55 +0300

    Created projects index

---
+++ src/data/projects.js
@@ -0,0 +1,32 @@
+const projects = [
+    {
+        picturePath:"https://github.com/lidwan/File-System-Analyzer/blob/main/src/main/resources/sc.png?raw=true",
+        title:"File System Analyzer",
+        description:"A File System Analyzer that Scannes directories, breaks down disk usage, detects duplicate files, and has a GUI with charts to better visualize information, and much more!",
+        ghLink:"https://github.com/lidwan/File-System-Analyzer",
+        livePreviewLink:""
+    },
+    {
+        picturePath:"",
+        title:"Mini search engine",
+        description:"Scrapes a website then lets the user query and returns the top 10 results from website.",
+        ghLink:"https://github.com/lidwan/Mini-Search-Engine",
+        livePreviewLink:""
+    },
+    {
+        picturePath:"",
+        title:"Arch linux & hyperland config.",
+        description:"Personal config. files with a bash script to apply config and install a list of system packages and flatpaks.",
+        ghLink:"https://github.com/lidwan/dotfiles",
+        livePreviewLink:""
+    },
+    {
+        picturePath:"",
+        title:"My Blog",
+        description:"A Blog where I share thoughts on tech, coding, and open source. Made with React, author writes the post in a text editor and uploads .md file, and updates the posts index, the website does the rest!",
+        ghLink:"https://github.com/lidwan/blog",
+        livePreviewLink:"https://blog.loayidwan.com/"
+    }
+];
+
+export default projects
