d8574d2562be07fa55d0e6c42d484496ff06e4b6
lidwan <contact@loayidwan.com>
2025-02-15 18:07:13 +0300

    Created Contact component

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,16 @@
+import './contact.css'
+
+const Contact = () => {
+
+
+    return (
+
+        <div className="wholePageSection centerContainer">
+            <h1 className="getInTouch">Get in touch</h1>
+            <h3>Email: <a href="mailto:info@loayidwan.com" className="link">info@loayidwan.com</a></h3>
+            <h3>Whatsapp: <a href="https://wa.me/+962790945507" className="link">+962790945507</a></h3>
+        </div>
+    )
+}
+
+export default Contact
