a2c22503c588caeb05e0c01aa2e39a6d817995e3
lidwan <contact@loayidwan.com>
2025-02-15 19:48:28 +0300

    Updated App.jsx to use all components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import NavBar from "./components/NavBar/NavBar.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import Greeting from "./components/Greeting/Greeting.jsx";
 import About from "./components/About/About.jsx";
+import Contact from "./components/Contact/Contact.jsx";
+import ScrollUp from "./components/ScrollUp/ScrollUp.jsx";
+import Projects from "./components/Projects/Projects.jsx";
 
 function App() {
 
@@ -14,6 +17,13 @@ function App() {
         <div>
             <About />
         </div>
+        <div>
+            <Projects />
+        </div>
+        <div>
+            <Contact />
+        </div>
+        <ScrollUp />
         <Footer/>
     </>
   )
