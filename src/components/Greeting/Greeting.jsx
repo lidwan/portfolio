5ebef87343434a905817ca8c4183ef9a1ea00e97
Loay Idwan <contact@loayidwan.com>
2026-01-06 10:30:00 +0300

    Modified Projects

--- src/components/Greeting/Greeting.jsx
+++ src/components/Greeting/Greeting.jsx
@@ -5,7 +5,7 @@ const Greeting = () => {
         <div className="greetingContainer wholePageSection">
             <h1>Hello,<br/>
                 world.</h1>
-            <h3>My name is Loay Idwan, I&#39;m a third year CIS student <br/>
+            <h3>My name is Loay Idwan, I&#39;m a fourth year CIS student <br/>
                 at <a href="https://just.edu.jo/" >JUST</a>, check out <a href="#projects">my projects</a>, and <a href="https://blog.loayidwan.com/">my tech blog</a></h3>
         </div>
     );
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,28 +1,9 @@
 const projects = [
     {
-        picturePath:"https://github.com/lidwan/todo/blob/main/public/todo.png?raw=true",
-        title:"Todo web app",
-        description:"A simple Todo web app built with Next.js, Clerk, SupaBase (PostgreSQL DB w/ RLS), shadcn/ui, Tailwind CSS, TypeScript",
-        ghLink:"https://github.com/lidwan/todo",
-        liveLink:"https://todo.loayidwan.com"
-    },
-    {
         picturePath:"https://github.com/lidwan/File-System-Analyzer/blob/main/src/main/resources/sc.png?raw=true",
         title:"File System Analyzer",
         description:"A File System Analyzer that Scannes directories, breaks down disk usage, detects duplicate files, and has a GUI with charts to better visualize information, and much more!",
         ghLink:"https://github.com/lidwan/File-System-Analyzer"
-    },
-    {
-        picturePath:"/images/mse.png",
-        title:"Mini search engine",
-        description:"Scrapes a website then lets the user query and returns the top 10 results from website.",
-        ghLink:"https://github.com/lidwan/Mini-Search-Engine"
-    },
-    {
-        picturePath:"/images/arch.png",
-        title:"Arch linux & hyperland config.",
-        description:"Personal config. files with a bash script to apply config and install a list of system packages and flatpaks.",
-        ghLink:"https://github.com/lidwan/dotfiles"
     },
     {
         picturePath:"/images/blog.png",
@@ -30,6 +11,12 @@ const projects = [
         description:"A Blog where I share thoughts on tech, coding, and open source. Using React Markdown, adding new posts is very easy.",
         ghLink:"https://github.com/lidwan/blog",
         liveLink:"https://blog.loayidwan.com"
+    },
+    {
+        picturePath:"/images/arch.png",
+        title:"Arch linux & hyperland config.",
+        description:"Personal config. files with a bash script to apply config and install a list of system packages and flatpaks.",
+        ghLink:"https://github.com/lidwan/dotfiles"
     }
 ];
 
