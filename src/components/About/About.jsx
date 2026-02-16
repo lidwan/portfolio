7ea920b0e84c5494ded348619103e6b072b2c318
Loay Idwan <contact@loayidwan.com>
2026-02-16 22:04:45 +0300

    restyle navbar hero and about sections

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,17 +4,17 @@ const About = () => {
 
 
     return (
-        <div className="wholePageSection centerContainer">
+        <section className="wholePageSection centerContainer aboutContainer">
             <div id="about">&nbsp;</div>
             <h1 className="AboutLI">About Loay Idwan</h1>
-            <h4>
+            <h4 className="aboutText">
                 I’m a cat-loving, Linux enthusiast and hobby photographer, <br/>
                 currently juggling life as a student in Jordan. <br/>
                 I’m diving deep into Spring for backend and React for frontend. <br/>
                 I also enjoy sharing tech insights on <a href="https://https://blog.loayidwan.com/">my blog</a>.<br/>
                 all while managing a two-hour commute to university.
             </h4>
-        </div>
+        </section>
     )
 }
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,31 +2,31 @@ import './navbar.css'
 
 const NavBar = () => {
     return (
-        <nav className="navbar navbar-expand-lg bg-black navbar-dark">
-            <div className="container-fluid">
+        <nav className="navbar navbar-expand-lg navbar-dark siteNav">
+            <div className="container-fluid navInner">
                 <a className="navbar-brand" id="title" href="https://loayidwan.com/">Loay Idwan</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav ms-auto">
+                    <ul className="navbar-nav ms-auto navLinksList">
 
                         <li className="nav-item">
-                            <a className="nav-link active" href="#">Home</a>
+                            <a className="nav-link siteNavLink active" href="#">Home</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#about">About</a>
+                            <a className="nav-link siteNavLink" href="#about">About</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" aria-current="page"
+                            <a className="nav-link siteNavLink" aria-current="page"
                                href="https://blog.loayidwan.com/">Blog</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#projects">Projects</a>
+                            <a className="nav-link siteNavLink" href="#projects">Projects</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#contact">Contact</a>
+                            <a className="nav-link siteNavLink" href="#contact">Contact</a>
                         </li>
                     </ul>
                 </div>
