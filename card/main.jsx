f2810adf4a90460064d753a80e31542bd1f01da9
Loay Idwan <contact@loayidwan.com>
2026-02-20 00:35:32 +0300

    fixed hamburger menu in /card in mobile view

--- card/main.jsx
+++ card/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Card from '../src/pages/Card/Card.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import '../src/index.css'
 
 createRoot(document.getElementById('root')).render(
