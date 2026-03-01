f8e90d44bb3014ac0f37c330552a41078b7c4288
Loay Idwan <contact@loayidwan.com>
2026-03-02 01:12:51 +0300

    Fixed scroll up button

--- src/components/ScrollUp/ScrollUp.jsx
+++ src/components/ScrollUp/ScrollUp.jsx
@@ -1,9 +1,21 @@
 import './scrollUp.css'
 
 const ScrollUp = () => {
+    const scrollToTop = () => {
+        if (typeof window === "undefined") return;
+
+        try {
+            window.scrollTo({ top: 0, behavior: "smooth" });
+        } catch {
+            window.scrollTo(0, 0);
+        }
+    };
+
     return (
         <div className="scrollUpButtonContainer">
-            <a href="#"><button type="button" className="btn btn-dark scrollTopButton">Scroll back up?</button></a>
+            <button type="button" className="scrollTopButton" onClick={scrollToTop}>
+                Scroll back up?
+            </button>
         </div>
     )
 }
